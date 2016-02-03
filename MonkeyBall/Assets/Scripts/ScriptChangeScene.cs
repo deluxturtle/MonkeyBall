275bd4c84b90e09ede4cfda3e531f8f33bd2b6d7
deluxturtle <sebaturtle@gmail.com>
2016-02-02 18:26:58 -0600

    Started making the menu systems

---
+++ MonkeyBall/Assets/Scripts/ScriptChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptChangeScene : MonoBehaviour {
+
+	public void _MainMenu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+
+    public void _SelectLevel()
+    {
+        Application.LoadLevel("LevelSelect");
+    }
+
+    public void _LevelOne()
+    {
+        Application.LoadLevel("LevelOne");
+    }
+}
