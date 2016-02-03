8b3d08fa81af748eae59cbb6b2c5d7ccd0913426
deluxturtle <sebaturtle@gmail.com>
2016-02-02 19:10:43 -0600

    Level1 Complete

--- MonkeyBall/Assets/Scripts/ScriptChangeScene.cs
+++ MonkeyBall/Assets/Scripts/ScriptChangeScene.cs
@@ -8,6 +8,11 @@ public class ScriptChangeScene : MonoBehaviour {
         Application.LoadLevel("MainMenu");
     }
 
+    public void _Store()
+    {
+        Application.LoadLevel("Store");
+    }
+
     public void _SelectLevel()
     {
         Application.LoadLevel("LevelSelect");
@@ -16,5 +21,53 @@ public class ScriptChangeScene : MonoBehaviour {
     public void _LevelOne()
     {
         Application.LoadLevel("LevelOne");
+    }
+
+    public void _LevelTwo()
+    {
+        Application.LoadLevel("LevelTwo");
+    }
+
+    public void _LevelThree()
+    {
+        Application.LoadLevel("LevelThree");
+    }
+
+    public void _LevelFour()
+    {
+        Application.LoadLevel("LevelFour");
+    }
+
+    public void _LevelFive()
+    {
+        Application.LoadLevel("LevelFive");
+    }
+
+    public void _Quit()
+    {
+        Application.Quit();
+    }
+
+    public void _NextLevel()
+    {
+        switch (Application.loadedLevelName)
+        {
+            case "LevelOne":
+                Application.LoadLevel("LevelTwo");
+                break;
+            case "LevelTwo":
+                Application.LoadLevel("LevelThree");
+                break;
+            case "LevelThree":
+                Application.LoadLevel("LevelFour");
+                break;
+            case "LevelFour":
+                Application.LoadLevel("LevelFive");
+                break;
+            case "LevelFive":
+                Application.LoadLevel("MainMenu");
+                break;
+
+        }
     }
 }
